c72b2c5575d94bfbe12aa2d7f54794d3153d8391
Ahmad N. F <fahmadnaufal@gmail.com>
2021-04-17 22:31:50 +0700

    refactor some logic; revert to threadbased

--- main.py
+++ main.py
@@ -12,9 +12,8 @@ q = queue.Queue()
 hmap = {}
 
 class PlaylistFetcherThread(threading.Thread):
-  def __init__(self, threadID, name, playlist_url, output_dir):
+  def __init__(self, name, playlist_url, output_dir):
     super(PlaylistFetcherThread, self).__init__()
-    self.threadID = threadID
     self.name = name
     self.playlist_url = playlist_url
     self.playlist_filename = 'chunklist.m3u8'
@@ -29,20 +28,23 @@ class PlaylistFetcherThread(threading.Thread):
         continue
 
       for segment in playlist.segments:
-        ts_props = (segment.base_uri, segment.uri)
-        if segment.uri in hmap:
-          logging.info("%s is already on the list" % (segment.uri))
+        filename = segment.uri.replace('/', '-')
+        download_uri = segment.base_uri + segment.uri
+        ts_props = (download_uri, filename)
+        if filename in hmap:
+          logging.info("%s is already on the list" % (filename))
           continue
 
         q.put(ts_props)
-        hmap[segment.uri] = True
+        hmap[filename] = True
 
       playlist.dump(self.output_dir + self.playlist_filename)
       time.sleep(playlist.target_duration)
 
 
-class Downloader(object):
+class DownloaderThread(threading.Thread):
   def __init__(self, name, output_dir):
+    super(DownloaderThread, self).__init__()
     self.name = name
     self.ts_list_filename = 'list.txt'
     self.counter = 0
@@ -51,18 +53,18 @@ class Downloader(object):
     self.num_retries = 3
     self.output_dir = output_dir
 
-  def run_download(self):
+  def run(self):
     while True:
       filename = self.dequeue_download()
       self.insert_to_batch(filename)
 
   def dequeue_download(self):
-    base_uri, filename = q.get(block=True, timeout=30)
+    download_uri, filename = q.get(block=True, timeout=30)
 
     success = False
     for i in range(self.num_retries):
-      resp = requests.get(base_uri + filename)
-      logging.info("Consumer - Request to %s (Retries: %d)" % (base_uri+filename, i))
+      resp = requests.get(download_uri)
+      logging.info("Consumer - Request to %s (Retries: %d)" % (download_uri, i))
 
       if resp.status_code == 200:
         success = True
@@ -71,7 +73,7 @@ class Downloader(object):
     if not success:
       raise "Error after attempting to retry"
 
-    with open(self.output_dir + (filename.split('/')[-1]), 'wb') as fd:
+    with open(self.output_dir + filename, 'wb') as fd:
       for chunk in resp.iter_content(chunk_size=128):
         fd.write(chunk)
 
@@ -80,18 +82,18 @@ class Downloader(object):
     return filename
 
   def insert_to_batch(self, ts_filename):
-      self.counter += 1
-      self.batch_list.append(ts_filename)
+    self.counter += 1
+    self.batch_list.append(ts_filename)
 
-      # flush into output list ts file for each batch
-      if self.counter % self.write_to_list_batch_size == 0:
-        self.dump_batch_to_file()
-        self.batch_list.clear()
+    # flush into output list ts file for each batch
+    if self.counter % self.write_to_list_batch_size == 0:
+      self.dump_batch_to_file()
+      self.batch_list.clear()
 
   def dump_batch_to_file(self):
-      with open(self.output_dir + self.ts_list_filename, 'a+') as list_fd:
-        for filename in self.batch_list:
-          list_fd.write("file '%s'\n" % (filename))
+    with open(self.output_dir + self.ts_list_filename, 'a+') as list_fd:
+      for filename in self.batch_list:
+        list_fd.write("file '%s'\n" % (filename))
 
 
 def main():
@@ -108,8 +110,8 @@ def main():
 
   args = parser.parse_args()
 
-  downloader = Downloader("downloader", args.odir)
-  fetcher = PlaylistFetcherThread(2, "fetcher", args.playlist_url, args.odir)
+  downloader = DownloaderThread("downloader", args.odir)
+  fetcher = PlaylistFetcherThread("fetcher", args.playlist_url, args.odir)
 
   logging.basicConfig(level=logging.INFO)
 
@@ -117,14 +119,13 @@ def main():
     logging.info("Wait until %d (current time: %f)" % (args.start_at, datetime.now().timestamp()))
     time.sleep(1)
 
-  try:
-    fetcher.start()
-    downloader.run_download()
-  except Exception as err:
-    logging.info("Error thrown: %s" % (err))
-  finally:
-    logging.info("Download completed.")
-    fetcher.join()
+  fetcher.start()
+  downloader.start()
+
+  fetcher.join()
+  downloader.join()
+
+  logging.info("Download completed.")
 
 
 if __name__ == '__main__':
